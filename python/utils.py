649e6a5194edddaad1abfdc0358234ce85d6d526
Long Le <longle2718@gmail.com>
2017-04-30 18:56:19 -0500

    added utils.py

---
+++ python/utils.py
@@ -0,0 +1,15 @@
+'''
+Utility functions
+
+Long Le <longle1@illinois.edu>
+University of Illinois
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import multiprocessing
+from joblib import Parallel, delayed
+
+def delay2loc_grad():
+    # from delays to location using gradient descent
+
+    return
