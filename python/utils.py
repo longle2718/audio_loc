81b6389490fbe9affe2a8009f8d9dcafe9dd0b91
Long Le <longle2718@gmail.com>
2017-04-30 20:31:30 -0500

    working on utils.py

--- python/utils.py
+++ python/utils.py
@@ -9,7 +9,25 @@ import matplotlib.pyplot as plt
 import multiprocessing
 from joblib import Parallel, delayed
 
-def delay2loc_grad():
+cSound = 343 # speed of sound m/s
+
+def delay2loc_grad(micsloc,meas_delays):
     # from delays to location using gradient descent
+    # micsloc: Mx3, 3-D locations of M mics
+    # meas_delays: MxM, measured delays
+
+    mu = 1. # gradient step size
+
+    startloc = np.mean(micsloc,axis=0)
+    locnow = startloc
+
+    for k in range(128):
+        delays = estDelays(micsloc,locnow,fs)
+
+    return
+
+def estDelays(micsloc,srcloc,fs):
+    # estimate theoretical delays given a 
+    # source location srcloc and the array micsloc
 
     return
