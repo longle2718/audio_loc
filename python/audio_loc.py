5415caec3039be5e8e5b42955253ecb025757a12
Long N. Le <longle1@illinois.edu>
2017-05-03 18:40:52 -0500

    working on segProc()

--- python/audio_loc.py
+++ python/audio_loc.py
@@ -16,37 +16,76 @@ sys.path.append('../../audio_class/python')
 import audio_class
 import ridgeDTW
 
-def ridgePar(k,data,fs,tBlk,tInc,tBkt):
-    S,F,T,tBlk,tInc = audio_class.spectrographic(data[k,:],fs,tBlk,tInc)
+def extrSpec(data,fs,tBlk,tInc):
+    # routine for extracting spectrogram
+    # tBlk: block time
+    # tInc: increment time
+    #
+    S,F,T,tBlk,tInc = audio_class.spectrographic(data,fs,tBlk,tInc)
+    return S,F,T,tBlk,tInc
+
+def extrRidge(S,tInc,tBkt):
+    # (parallel) routine for extracting ridges
+    # tBkt: backtrack time
+    #
     X = ridgeDTW.ridgeTracker(S,np.median(S.flatten()),tBkt,tInc,isMaxPool=False,supThresh=3.)
-    return T,F,X
+    return X,T,F
+
+def pool(X,T,F,tSegBlk,tSegInc,tInc):
+    # aggregate the input representation
+    # to form the output representation
+    #
+    NF,NT = np.shape(X)
+    fBlk = int(tSegBlk/tInc) # frame block in a segment
+    fInc = int(tSegInc/tInc) # frame increment between consecutive segments
+    NSeg = int(np.ceil(NT/fInc))
+
+    FF = F
+    # #-of-segs x #-of-frames-per-seg x #-of-seconds-per-frame
+    TT = np.arange(NSeg)*fInc*tInc 
+    XX = np.zeros((NF,nSeg))
+    i = 0
+    for k in range(nSeg):
+        j = min(NT,i+fBlk)
+
+        XX[:,k] = np.sum(np.pad(X[:,i:j],(NF,fBlk-(j-i)),'constant',constant_values=0),axis=1)
+
+        i = min(NT,i+fInc)
+    
+    return XX,TT,FF
 
 def segProc(data,fs,tSegBlk=2.0,tSegInc=1.0,tBlk=0.032,tInc=0.004,tBkt=0.008,debug=0):
     # segment-processing the multi-channel data at
     # frame scale (parameterized by tBlk and tInc in seconds) and
     # segment scale (parameterized by tSegBlk and tSegInc in seconds)
     #
-    nCh,_ = np.shape(data)
-    rvs = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(ridgePar)(k,data,fs,tBlk,tInc,tBkt) for k in range(nCh))
+    NCh,_ = np.shape(data)
+
+    rvSpec = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrSpec)(data[k,:],fs,tBlk,tInc) for k in range(NCh))
+
+    rvRidge = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrRidge)(rvSpec[k][0],tInc,tBkt) for k in range(NCh))
 
     # plot for debugging
     if debug >= 1:
-        T,F,X = rvs[7]
+        T,F,X = rvRidge[7]
         tInc = T[1]-T[0]
         n1 = int(35/tInc)#12000
-        n2 = int(35/tInc)#12800
+        n2 = int(37/tInc)#12800
         print('n1 = %s, n2 = %s' % (n1,n2))
         plt.figure(figsize=(15,15))
         plt.pcolormesh(T[n1:n2],F,np.sqrt(X[:,n1:n2]))
         plt.show()
     if debug >= 2:
         plt.figure(figsize=(15,30))
-        for k in range(nCh):
-            T,F,X = rvs[k]
-            plt.subplot(nCh,1,1+k)
+        for k in range(NCh):
+            T,F,X = rvRidge[k]
+            plt.subplot(NCh,1,1+k)
             plt.pcolormesh(T[n1:n2],F,np.sqrt(X[:,n1:n2]))
         plt.show()
 
-    # segmenting the frames
+    # group frames into segments
+    rvRidge1 = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(pool)(rvRidge[k][0],rvRidge[k][1],rvRidge[k][2],tSegBlk,tSegInc,tInc) for k in range(NCh))
+
+    rvRidge2 = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrRidge)(rvRidge1[k][0],tInc,tBkt) for k in range(NCh))
 
     return
