08f9892d2909b7ffde299976a4c8d51d9455acf9
Long N. Le <longle1@illinois.edu>
2017-05-04 15:06:58 -0500

    coded hierarchical processing

--- python/audio_loc.py
+++ python/audio_loc.py
@@ -24,68 +24,54 @@ def extrSpec(data,fs,tBlk,tInc):
     S,F,T,tBlk,tInc = audio_class.spectrographic(data,fs,tBlk,tInc)
     return S,F,T,tBlk,tInc
 
-def extrRidge(S,tInc,tBkt):
-    # (parallel) routine for extracting ridges
-    # tBkt: backtrack time
-    #
-    X = ridgeDTW.ridgeTracker(S,np.median(S.flatten()),tBkt,tInc,isMaxPool=False,supThresh=3.)
-    return X,T,F
-
-def pool(X,T,F,tSegBlk,tSegInc,tInc):
+def pool(S,F,T,tSegBlk,tSegInc,tInc):
     # aggregate the input representation
     # to form the output representation
     #
-    NF,NT = np.shape(X)
-    fBlk = int(tSegBlk/tInc) # frame block in a segment
-    fInc = int(tSegInc/tInc) # frame increment between consecutive segments
+    NF,NT = np.shape(S)
+    fBlk = int(tSegBlk/tInc) # # of frames in a segment
+    tSegBlk = fBlk*tInc
+    fInc = int(tSegInc/tInc) # # of frames incremented between consecutive segments
+    tSegInc = fInc*tInc
     NSeg = int(np.ceil(NT/fInc))
 
     FF = F
     # #-of-segs x #-of-frames-per-seg x #-of-seconds-per-frame
     TT = np.arange(NSeg)*fInc*tInc 
-    XX = np.zeros((NF,nSeg))
+    SS = np.zeros((NF,NSeg))
     i = 0
-    for k in range(nSeg):
+    for k in range(NSeg):
         j = min(NT,i+fBlk)
 
-        XX[:,k] = np.sum(np.pad(X[:,i:j],(NF,fBlk-(j-i)),'constant',constant_values=0),axis=1)
+        SS[:,k] = np.mean(np.pad(S[:,i:j],((0,0),(0,fBlk-(j-i))),'constant',constant_values=0),axis=1)
 
         i = min(NT,i+fInc)
     
-    return XX,TT,FF
+    return SS,FF,TT,tSegBlk,tSegInc
 
-def segProc(data,fs,tSegBlk=2.0,tSegInc=1.0,tBlk=0.032,tInc=0.004,tBkt=0.008,debug=0):
-    # segment-processing the multi-channel data at
+def extrRidge(S,tInc,bktRatio):
+    # (parallel) routine for extracting ridges
+    # bktRatio: backtrack ratio
+    #
+    tBkt = tInc*bktRatio # backtrack time
+    X = ridgeDTW.ridgeTracker(S,np.median(S.flatten()),tBkt,tInc,isMaxPool=False,supThresh=5.5)
+    return X
+
+def hieProc(data,fs,tHieBlk=[0.032,2.0],tHieInc=[0.004,1.0]):
+    # hierarchical-processing the multi-channel data at
     # frame scale (parameterized by tBlk and tInc in seconds) and
     # segment scale (parameterized by tSegBlk and tSegInc in seconds)
     #
     NCh,_ = np.shape(data)
+    NHie = len(tHieBlk)
+    hSpecs = [None]*NHie # hierarchical specs
+    hRidges = [None]*NHie # hierarchical ridges
+    bktRatio = 2
 
-    rvSpec = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrSpec)(data[k,:],fs,tBlk,tInc) for k in range(NCh))
+    hSpecs[0] = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrSpec)(data[k,:],fs,tHieBlk[0],tHieInc[0]) for k in range(NCh))
+    hRidges[0] = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrRidge)(hSpecs[0][k][0],hSpecs[0][k][-1],bktRatio) for k in range(NCh))
+    for l in range(1,NHie):
+        hSpecs[l] = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(pool)(hSpecs[l-1][k][0],hSpecs[l-1][k][1],hSpecs[l-1][k][2],tHieBlk[l],tHieInc[l],hSpecs[l-1][k][-1]) for k in range(NCh))
+        hRidges[l] = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrRidge)(hSpecs[l][k][0],hSpecs[l][k][-1],bktRatio) for k in range(NCh))
 
-    rvRidge = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrRidge)(rvSpec[k][0],tInc,tBkt) for k in range(NCh))
-
-    # plot for debugging
-    if debug >= 1:
-        T,F,X = rvRidge[7]
-        tInc = T[1]-T[0]
-        n1 = int(35/tInc)#12000
-        n2 = int(37/tInc)#12800
-        print('n1 = %s, n2 = %s' % (n1,n2))
-        plt.figure(figsize=(15,15))
-        plt.pcolormesh(T[n1:n2],F,np.sqrt(X[:,n1:n2]))
-        plt.show()
-    if debug >= 2:
-        plt.figure(figsize=(15,30))
-        for k in range(NCh):
-            T,F,X = rvRidge[k]
-            plt.subplot(NCh,1,1+k)
-            plt.pcolormesh(T[n1:n2],F,np.sqrt(X[:,n1:n2]))
-        plt.show()
-
-    # group frames into segments
-    rvRidge1 = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(pool)(rvRidge[k][0],rvRidge[k][1],rvRidge[k][2],tSegBlk,tSegInc,tInc) for k in range(NCh))
-
-    rvRidge2 = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(extrRidge)(rvRidge1[k][0],tInc,tBkt) for k in range(NCh))
-
-    return
+    return hRidges,hSpecs
